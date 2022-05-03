73a661f088b31d220e24a0cd3654f26fa879a5e0
Walt Boxwell <waltboxwell@gmail.com>
2022-05-02 20:18:03 -0500

    Node problem

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+//const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+//const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
-
-fs.writeFile('index.html',generatePage(name,github), err => {
-    if(err) throw err;
-
-    console.log('Portfolio complete! Checkout index.html to see the output');
-});
+//const [name, github] = profileDataArgs;
 
 
-jjj
+//fs.writeFile('index.html',pageHTML, err => {
+    //if(err) throw err;
+
+   // console.log('Portfolio complete! Checkout index.html to see the output');
+//});
